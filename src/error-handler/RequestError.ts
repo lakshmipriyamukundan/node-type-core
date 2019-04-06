ee6b16b6a59d063c89d89b02758efb4042168e58
lakshmipriyamukundan <lakshmipriya.mukundan@gmail.com>
2019-04-06 11:23:25 +0530

    deleted unwanted files

--- src/error-handler/RequestError.ts
+++
@@ -1,7 +0,0 @@
-import {
-  ExpressRequestError as RequestError,
-  ExpressRequestErrorType as RequestErrorType,
-  ERROR_DEFINITIONS,
-} from 'issue-maker';
-
-export { RequestError, RequestErrorType, ERROR_DEFINITIONS };
--- src/utils/get-route.ts
+++
@@ -1,8 +0,0 @@
-import { Request } from 'express';
-import * as UrlPattern from 'url-pattern';
-
-export const getRoute = (req: Request): string => {
-  const pattern = new UrlPattern(`*(${req.route.path})`);
-  const route = pattern.match(req.path);
-  return route._;
-};
