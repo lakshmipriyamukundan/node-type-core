b88c95527b45228ec005fb7056e11d12512136c6
lakshmipriyamukundan <lakshmipriya.mukundan@gmail.com>
2019-04-07 17:20:29 +0530

    winston config changes

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import * as morgan from 'morgan';
 import * as path from 'path';
 import * as favicon from 'serve-favicon';
 
-import { stream} from './config/app/winston';
+import { stream, logger} from './config/app/winston';
 
 import { apis } from './routes';
 export const app = express();
@@ -28,7 +28,7 @@ app.use(
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 if (morganEnabled) {
-  app.use(morgan('dev', { stream }));
+  app.use(morgan('combined', { stream }));
 }
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -37,40 +37,36 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/v1', apis);
 
+
+
 // error handler
-const requestErrHandler: express.ErrorRequestHandler = async (
+const requestErrHandler: express.ErrorRequestHandler = (
   err: any,
   req,
   res,
   next,
 ) => {
-  if (err.statusCode >= 500) {
-    // stdout to log
-    console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'); // tslint:disable-line
-    console.log(err); // tslint:disable-line
-    console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'); // tslint:disable-line
-  }
-  if (
-    err.statusCode >= 500 &&
-    process.env.NODE_ENV !== 'development' &&
-    process.env.TESTING !== 'true'
-  ) {
-    console.log('ERR: some error occurred while reporting issue', err); //tslint:disable-line
-  }
+  //if (err.statusCode >= 500) {
+  //   logger.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`)
+  //   // stdout to log
+  //   console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'); // tslint:disable-line
+  //   console.log(err); // tslint:disable-line
+  //   // console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'); // tslint:disable-line
+  // //}
 
-  if (req.xhr) {
-    // remove sensitive err details
-    if (err.statusCode >= 500) {
-      return res.status(err.statusCode).send({
-        status: 'failed',
-        message: err.message,
-        statusCode: err.statusCode,
-        remarks: 'This incident has reported.',
-      });
-    } else {
-      return res.status(err.statusCode).send(err);
-    }
-  }
+  // if (req.xhr) {
+  //   // remove sensitive err details
+  //   if (err.statusCode >= 500) {
+  //     return res.status(err.statusCode).send({
+  //       status: 'failed',
+  //       message: err.message,
+  //       statusCode: err.statusCode,
+  //       remarks: 'This incident has reported.',
+  //     });
+  //   } else {
+  //     return res.status(err.statusCode).send(err);
+  //   }
+  // }
 
   // set locals, only providing error in development
   res.locals.message = err.message;
--- src/config/app/winston.ts
+++ src/config/app/winston.ts
@@ -1,31 +1,20 @@
 import {get as getConfig } from 'config';
 import * as userHome from 'user-home';
 import * as path from 'path';
-import { createLogger, Logger, LoggerOptions, transports } from "winston";
+import { createLogger, Logger, LoggerOptions } from "winston";
 import winston = require('winston');
 const DailyRotateFile = require("winston-daily-rotate-file");
 
 const defaultLogLevel: string = getConfig('app.defaultLogLevel');
 
-const INFO_FILE_PATH = path.join(userHome, 'Projects', 'type-core', 'info.log');
-const ERROR_FILE_PATH = path.join(userHome, 'Projects', 'type-core', 'error.log');
-const SILLY_FILE_PATH = path.join(userHome, 'Projects', 'type-core', 'silly.log');
+// const INFO_FILE_PATH = path.join(userHome, 'Projects', 'type-core','logs', 'info.log');
+const ERROR_FILE_PATH = path.join(userHome, 'Projects', 'type-core', 'logs','error.log');
+// const SILLY_FILE_PATH = path.join(userHome, 'Projects', 'type-core', 'logs', 'silly.log');
 
 const options: LoggerOptions = {
     exitOnError: false,
     level: defaultLogLevel,
     transports: [
-        new DailyRotateFile({
-            name: "info",
-            filename: INFO_FILE_PATH,
-            datePattern: "YYYY-MM-DD-HH",
-            zippedArchive: true,
-            maxSize: "20m",
-            maxFiles: "14d",
-            showLevel: true,
-            timestamp: true,
-            level: "info", // info and below to rotate
-        }),
         new DailyRotateFile({
             name: "error",
             filename: ERROR_FILE_PATH,
@@ -37,17 +26,6 @@ const options: LoggerOptions = {
             timestamp: true,
             level: "error", // error and below to rotate
         }),
-        new DailyRotateFile({
-            name: "silly",
-            filename: SILLY_FILE_PATH,
-            datePattern: "YYYY-MM-DD-HH",
-            zippedArchive: true,
-            maxSize: "20m",
-            maxFiles: "1d",
-            showLevel: true,
-            timestamp: true,
-            level: "silly", // error and below to rotate
-        }),
     ],
 };
 
@@ -55,29 +33,13 @@ const logger: Logger  = createLogger(options);
 
 if (process.env.NODE_ENV === 'development') {
     logger.add(new winston.transports.Console({
-        level: "debug", // debug and below to console
+        level: 'debug',
     }));
 }
 
-// logger.add(new winston.transports.Stream({
-//     stream: {
-//         write: (message: string) => {
-//             logger.info(message)
-//         }
-//     }
-//   }));
-
-// create a stream object with a 'write' function that will be used by `morgan`
-// logger.stream = {
-//     write: (message: string, encoding: string)=> {
-//       // use the 'info' log level so the output will be picked up by both transports (file and console)
-//       logger.info(message);
-//     },
-//   };
-
 export const stream = {
     write: (message: string) => {
-      logger.info(message);
+      logger.debug(message);
     },
   };
 
